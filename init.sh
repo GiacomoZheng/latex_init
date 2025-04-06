4ad53d3bed1722012c101cd58544a86c12fc790d
Giacomo Zheng <giacomo821@outlook.com>
2025-04-06 16:42:22 +0100

    update ref

--- init.sh
+++ init.sh
@@ -7,6 +7,13 @@ BLUE="\033[34m"
 NC='\033[0m'
 
 source="/Users/giacomo/storage/GitHub/latex_init"
+
+# 如果需要更新ref.bib
+if [ $1 = "ref" ]; then
+    code $source/ref.bib
+    exit 1
+fi
+
 # 检查settings.tex文件是否存在
 if [ -f "./settings.tex" ]; then
     if [ $1 = "-f" ]; then
@@ -23,12 +30,36 @@ else
     echo "settings.tex has been copied to the current folder."
 fi
 
-# 检查ref.bib文件是否存在
-if ! [ -f "./ref.bib" ]; then
-    # 生成空的ref.bib文件
-	touch ./ref.bib
-	echo "An empty 'ref.bib' file has been created in the current folder."
+############################################################
+
+# 检查模板文件是否存在
+if [ ! -f "$source/ref.bib" ]; then
+    echo "Template ref.bib does not exist!"
+    exit 1
 fi
+# 如果当前目录下的 ref.bib 不存在，则直接复制
+if [ ! -f "./ref.bib" ]; then
+    cp "$source/ref.bib" "./ref.bib"
+    echo "ref.bib did not exist, so it has been copied from the template."
+else
+    # 获取修改时间（以 Unix 时间戳表示）
+    TEMPLATE_MOD_TIME=$(stat -f %m "$source/ref.bib")
+    CURRENT_MOD_TIME=$(stat -f %m "./ref.bib")
+
+    # 比较时间戳
+    if [ "$TEMPLATE_MOD_TIME" -gt "$CURRENT_MOD_TIME" ]; then
+        chmod 644 "./ref.bib"
+        cp "$source/ref.bib" "./ref.bib"
+        echo "Template ref.bib is newer. Updated ref.bib."
+    else
+        echo "Current ref.bib is up to date. No update needed."
+    fi
+fi
+# 设置 ref.bib 为只读
+chmod 444 "./ref.bib"
+# echo "Set ref.bib to read-only mode."
+
+############################################################
 
 # 检查main.tex文件是否存在
 if ! [ -f "./main.tex" ]; then
